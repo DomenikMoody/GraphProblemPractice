71ebacc61e776a71a812a7fc2c2e4591c0908386
Alan Nguyen <alanngn21@gmail.com>
2023-02-02 14:28:19 -0800

    island done

--- island.js
+++ island.js
@@ -25,15 +25,26 @@ return coors
 function islandSize(row, col, graph) {
 
   // Create a visited set to store visited nodes
-
+  const visited = new Set();
   // Create a stack, put the starting node in the stack
-
+  const stack = [[row,col]]
   // Put the stringified starting node in visited
-
+  visited.add(stack[0].toString())
   // Initialize size to 0
-
+  let size = 0
   // While the stack is not empty,
-
+  while(stack.length){
+    let curr = stack.pop()
+    size ++;
+    const neighbors = getNeighbors(curr[0], curr[1], graph)
+    neighbors.forEach(neighbor => {
+      if(!visited.has(neighbor.toString())){
+        stack.push(neighbor)
+        visited.add(neighbor.toString())
+      }
+    })
+  }
+  return size;
     // Pop the first node
 
     // DO THE THING (increment size by 1)
